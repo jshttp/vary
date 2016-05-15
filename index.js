6661827f84be75921d091f1a230869525f4bdb3d
Douglas Christopher Wilson <doug@somethingdoug.com>
2016-05-15 15:43:02 -0400

    lint: use standard style

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
  * MIT Licensed
  */
 
-'use strict';
+'use strict'
 
 /**
  * Module exports.
  */
 
-module.exports = vary;
-module.exports.append = append;
+module.exports = vary
+module.exports.append = append
 
 /**
  * RegExp to match field-name in RFC 7230 sec 3.2
@@ -35,54 +35,54 @@ var fieldNameRegExp = /^[!#$%&'\*\+\-\.\^_`\|~0-9A-Za-z]+$/
  * @api public
  */
 
-function append(header, field) {
+function append (header, field) {
   if (typeof header !== 'string') {
-    throw new TypeError('header argument is required');
+    throw new TypeError('header argument is required')
   }
 
   if (!field) {
-    throw new TypeError('field argument is required');
+    throw new TypeError('field argument is required')
   }
 
   // get fields array
   var fields = !Array.isArray(field)
     ? parse(String(field))
-    : field;
+    : field
 
   // assert on invalid field names
-  for (var i = 0; i < fields.length; i++) {
-    if (!fieldNameRegExp.test(fields[i])) {
-      throw new TypeError('field argument contains an invalid header name');
+  for (var j = 0; j < fields.length; j++) {
+    if (!fieldNameRegExp.test(fields[j])) {
+      throw new TypeError('field argument contains an invalid header name')
     }
   }
 
   // existing, unspecified vary
   if (header === '*') {
-    return header;
+    return header
   }
 
   // enumerate current values
-  var val = header;
-  var vals = parse(header.toLowerCase());
+  var val = header
+  var vals = parse(header.toLowerCase())
 
   // unspecified vary
   if (fields.indexOf('*') !== -1 || vals.indexOf('*') !== -1) {
-    return '*';
+    return '*'
   }
 
   for (var i = 0; i < fields.length; i++) {
-    var fld = fields[i].toLowerCase();
+    var fld = fields[i].toLowerCase()
 
     // append value (case-preserving)
     if (vals.indexOf(fld) === -1) {
-      vals.push(fld);
+      vals.push(fld)
       val = val
         ? val + ', ' + fields[i]
-        : fields[i];
+        : fields[i]
     }
   }
 
-  return val;
+  return val
 }
 
 /**
@@ -93,8 +93,8 @@ function append(header, field) {
  * @api private
  */
 
-function parse(header) {
-  return header.trim().split(/ *, */);
+function parse (header) {
+  return header.trim().split(/ *, */)
 }
 
 /**
@@ -105,20 +105,20 @@ function parse(header) {
  * @api public
  */
 
-function vary(res, field) {
+function vary (res, field) {
   if (!res || !res.getHeader || !res.setHeader) {
     // quack quack
-    throw new TypeError('res argument is required');
+    throw new TypeError('res argument is required')
   }
 
   // get existing header
   var val = res.getHeader('Vary') || ''
   var header = Array.isArray(val)
     ? val.join(', ')
-    : String(val);
+    : String(val)
 
   // set new header
   if ((val = append(header, field))) {
-    res.setHeader('Vary', val);
+    res.setHeader('Vary', val)
   }
 }
--- test/test.js
+++ test/test.js
@@ -1,414 +1,414 @@
 
-var assert = require('assert');
-var http = require('http');
-var request = require('supertest');
-var vary = require('..');
+var assert = require('assert')
+var http = require('http')
+var request = require('supertest')
+var vary = require('..')
 
 describe('vary(res, field)', function () {
   describe('arguments', function () {
     describe('res', function () {
       it('should be required', function () {
-        assert.throws(vary.bind(), /res.*required/);
-      });
+        assert.throws(vary.bind(), /res.*required/)
+      })
 
       it('should not allow non-res-like objects', function () {
-        assert.throws(vary.bind(null, {}), /res.*required/);
-      });
-    });
+        assert.throws(vary.bind(null, {}), /res.*required/)
+      })
+    })
 
     describe('field', function () {
       it('should be required', function (done) {
         request(createServer(callVary()))
         .get('/')
-        .expect(500, /field.*required/, done);
-      });
+        .expect(500, /field.*required/, done)
+      })
 
       it('should accept string', function (done) {
         request(createServer(callVary('foo')))
         .get('/')
-        .expect(200, done);
-      });
+        .expect(200, done)
+      })
 
       it('should accept array of string', function (done) {
         request(createServer(callVary(['foo', 'bar'])))
         .get('/')
-        .expect(200, done);
-      });
+        .expect(200, done)
+      })
 
       it('should accept string that is Vary header', function (done) {
         request(createServer(callVary('foo, bar')))
         .get('/')
-        .expect(200, done);
-      });
+        .expect(200, done)
+      })
 
       it('should not allow separator ":"', function (done) {
         request(createServer(callVary('invalid:header')))
         .get('/')
-        .expect(500, /field.*contains.*invalid/, done);
-      });
+        .expect(500, /field.*contains.*invalid/, done)
+      })
 
       it('should not allow separator " "', function (done) {
         request(createServer(callVary('invalid header')))
         .get('/')
-        .expect(500, /field.*contains.*invalid/, done);
-      });
-    });
-  });
+        .expect(500, /field.*contains.*invalid/, done)
+      })
+    })
+  })
 
   describe('when no Vary', function () {
     it('should set value', function (done) {
       request(createServer(callVary('Origin')))
       .get('/')
       .expect('Vary', 'Origin')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should set value with multiple calls', function (done) {
       request(createServer(callVary(['Origin', 'User-Agent'])))
       .get('/')
       .expect('Vary', 'Origin, User-Agent')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should preserve case', function (done) {
       request(createServer(callVary(['ORIGIN', 'user-agent', 'AccepT'])))
       .get('/')
       .expect('Vary', 'ORIGIN, user-agent, AccepT')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should not set Vary on empty array', function (done) {
       request(createServer(callVary([])))
       .get('/')
       .expect(shouldNotHaveHeader('Vary'))
-      .expect(200, done);
-    });
-  });
+      .expect(200, done)
+    })
+  })
 
   describe('when existing Vary', function () {
     it('should set value', function (done) {
       request(createServer(alterVary('Accept', 'Origin')))
       .get('/')
       .expect('Vary', 'Accept, Origin')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should set value with multiple calls', function (done) {
       var server = createServer(function (req, res) {
-       res.setHeader('Vary', 'Accept');
-       vary(res, 'Origin');
-       vary(res, 'User-Agent');
-      });
+        res.setHeader('Vary', 'Accept')
+        vary(res, 'Origin')
+        vary(res, 'User-Agent')
+      })
       request(server)
       .get('/')
       .expect('Vary', 'Accept, Origin, User-Agent')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should not duplicate existing value', function (done) {
       request(createServer(alterVary('Accept', 'Accept')))
       .get('/')
       .expect('Vary', 'Accept')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should compare case-insensitive', function (done) {
       request(createServer(alterVary('Accept', 'accEPT')))
       .get('/')
       .expect('Vary', 'Accept')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should preserve case', function (done) {
       request(createServer(alterVary('AccepT', ['accEPT', 'ORIGIN'])))
       .get('/')
       .expect('Vary', 'AccepT, ORIGIN')
-      .expect(200, done);
-    });
-  });
+      .expect(200, done)
+    })
+  })
 
   describe('when existing Vary as array', function () {
     it('should set value', function (done) {
       request(createServer(alterVary(['Accept', 'Accept-Encoding'], 'Origin')))
       .get('/')
       .expect('Vary', 'Accept, Accept-Encoding, Origin')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should not duplicate existing value', function (done) {
       request(createServer(alterVary(['Accept', 'Accept-Encoding'], ['accept', 'origin'])))
       .get('/')
       .expect('Vary', 'Accept, Accept-Encoding, origin')
-      .expect(200, done);
-    });
-  });
+      .expect(200, done)
+    })
+  })
 
   describe('when Vary: *', function () {
     it('should set value', function (done) {
       request(createServer(callVary('*')))
       .get('/')
       .expect('Vary', '*')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should act as if all values alread set', function (done) {
       request(createServer(alterVary('*', ['Origin', 'User-Agent'])))
       .get('/')
       .expect('Vary', '*')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should erradicate existing values', function (done) {
       request(createServer(alterVary('Accept, Accept-Encoding', '*')))
       .get('/')
       .expect('Vary', '*')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should update bad existing header', function (done) {
       request(createServer(alterVary('Accept, Accept-Encoding, *', 'Origin')))
       .get('/')
       .expect('Vary', '*')
-      .expect(200, done);
-    });
-  });
+      .expect(200, done)
+    })
+  })
 
   describe('when field is string', function () {
     it('should set value', function (done) {
       request(createServer(callVary('Accept')))
       .get('/')
       .expect('Vary', 'Accept')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should set value when vary header', function (done) {
       request(createServer(callVary('Accept, Accept-Encoding')))
       .get('/')
       .expect('Vary', 'Accept, Accept-Encoding')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should acept LWS', function (done) {
       request(createServer(callVary('  Accept     ,     Origin    ')))
       .get('/')
       .expect('Vary', 'Accept, Origin')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should handle contained *', function (done) {
       request(createServer(callVary('Accept,*')))
       .get('/')
       .expect('Vary', '*')
-      .expect(200, done);
-    });
-  });
+      .expect(200, done)
+    })
+  })
 
   describe('when field is array', function () {
     it('should set value', function (done) {
       request(createServer(callVary(['Accept', 'Accept-Language'])))
       .get('/')
       .expect('Vary', 'Accept, Accept-Language')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should ignore double-entries', function (done) {
       request(createServer(callVary(['Accept', 'Accept'])))
       .get('/')
       .expect('Vary', 'Accept')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should be case-insensitive', function (done) {
       request(createServer(callVary(['Accept', 'ACCEPT'])))
       .get('/')
       .expect('Vary', 'Accept')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should handle contained *', function (done) {
       request(createServer(callVary(['Origin', 'User-Agent', '*', 'Accept'])))
       .get('/')
       .expect('Vary', '*')
-      .expect(200, done);
-    });
+      .expect(200, done)
+    })
 
     it('should handle existing values', function (done) {
       request(createServer(alterVary('Accept, Accept-Encoding', ['origin', 'accept', 'accept-charset'])))
       .get('/')
       .expect('Vary', 'Accept, Accept-Encoding, origin, accept-charset')
-      .expect(200, done);
-    });
-  });
-});
+      .expect(200, done)
+    })
+  })
+})
 
 describe('vary.append(header, field)', function () {
   describe('arguments', function () {
     describe('header', function () {
       it('should be required', function () {
-        assert.throws(vary.append.bind(), /header.*required/);
-      });
+        assert.throws(vary.append.bind(), /header.*required/)
+      })
 
       it('should be a string', function () {
-        assert.throws(vary.append.bind(null, 42), /header.*required/);
-      });
-    });
+        assert.throws(vary.append.bind(null, 42), /header.*required/)
+      })
+    })
 
     describe('field', function () {
       it('should be required', function () {
-        assert.throws(vary.append.bind(null, ''), /field.*required/);
-      });
+        assert.throws(vary.append.bind(null, ''), /field.*required/)
+      })
 
       it('should accept string', function () {
-        assert.doesNotThrow(vary.append.bind(null, '', 'foo'));
-      });
+        assert.doesNotThrow(vary.append.bind(null, '', 'foo'))
+      })
 
       it('should accept string that is Vary header', function () {
-        assert.doesNotThrow(vary.append.bind(null, '', 'foo, bar'));
-      });
+        assert.doesNotThrow(vary.append.bind(null, '', 'foo, bar'))
+      })
 
       it('should accept array of string', function () {
-        assert.doesNotThrow(vary.append.bind(null, '', ['foo', 'bar']));
-      });
+        assert.doesNotThrow(vary.append.bind(null, '', ['foo', 'bar']))
+      })
 
       it('should not allow separator ":"', function () {
-        assert.throws(vary.append.bind(null, '', 'invalid:header'), /field.*contains.*invalid/);
-      });
+        assert.throws(vary.append.bind(null, '', 'invalid:header'), /field.*contains.*invalid/)
+      })
 
       it('should not allow separator " "', function () {
-        assert.throws(vary.append.bind(null, '', 'invalid header'), /field.*contains.*invalid/);
-      });
+        assert.throws(vary.append.bind(null, '', 'invalid header'), /field.*contains.*invalid/)
+      })
 
       it('should not allow non-token characters', function () {
-        assert.throws(vary.append.bind(null, '', 'invalid\nheader'), /field.*contains.*invalid/);
-        assert.throws(vary.append.bind(null, '', 'invalid\u0080header'), /field.*contains.*invalid/);
-      });
-    });
-  });
+        assert.throws(vary.append.bind(null, '', 'invalid\nheader'), /field.*contains.*invalid/)
+        assert.throws(vary.append.bind(null, '', 'invalid\u0080header'), /field.*contains.*invalid/)
+      })
+    })
+  })
 
   describe('when header empty', function () {
     it('should set value', function () {
-      assert.equal(vary.append('', 'Origin'), 'Origin');
-    });
+      assert.equal(vary.append('', 'Origin'), 'Origin')
+    })
 
     it('should set value with array', function () {
-      assert.equal(vary.append('', ['Origin', 'User-Agent']), 'Origin, User-Agent');
-    });
+      assert.equal(vary.append('', ['Origin', 'User-Agent']), 'Origin, User-Agent')
+    })
 
     it('should preserve case', function () {
-      assert.equal(vary.append('', ['ORIGIN', 'user-agent', 'AccepT']), 'ORIGIN, user-agent, AccepT');
-    });
-  });
+      assert.equal(vary.append('', ['ORIGIN', 'user-agent', 'AccepT']), 'ORIGIN, user-agent, AccepT')
+    })
+  })
 
   describe('when header has values', function () {
     it('should set value', function () {
-      assert.equal(vary.append('Accept', 'Origin'), 'Accept, Origin');
-    });
+      assert.equal(vary.append('Accept', 'Origin'), 'Accept, Origin')
+    })
 
     it('should set value with array', function () {
-      assert.equal(vary.append('Accept', ['Origin', 'User-Agent']), 'Accept, Origin, User-Agent');
-    });
+      assert.equal(vary.append('Accept', ['Origin', 'User-Agent']), 'Accept, Origin, User-Agent')
+    })
 
     it('should not duplicate existing value', function () {
-      assert.equal(vary.append('Accept', 'Accept'), 'Accept');
-    });
+      assert.equal(vary.append('Accept', 'Accept'), 'Accept')
+    })
 
     it('should compare case-insensitive', function () {
-      assert.equal(vary.append('Accept', 'accEPT'), 'Accept');
-    });
+      assert.equal(vary.append('Accept', 'accEPT'), 'Accept')
+    })
 
     it('should preserve case', function () {
-      assert.equal(vary.append('Accept', 'AccepT'), 'Accept');
-    });
-  });
+      assert.equal(vary.append('Accept', 'AccepT'), 'Accept')
+    })
+  })
 
   describe('when *', function () {
     it('should set value', function () {
-      assert.equal(vary.append('', '*'), '*');
-    });
+      assert.equal(vary.append('', '*'), '*')
+    })
 
     it('should act as if all values already set', function () {
-      assert.equal(vary.append('*', 'Origin'), '*');
-    });
+      assert.equal(vary.append('*', 'Origin'), '*')
+    })
 
     it('should erradicate existing values', function () {
-      assert.equal(vary.append('Accept, Accept-Encoding', '*'), '*');
-    });
+      assert.equal(vary.append('Accept, Accept-Encoding', '*'), '*')
+    })
 
     it('should update bad existing header', function () {
-      assert.equal(vary.append('Accept, Accept-Encoding, *', 'Origin'), '*');
-    });
-  });
+      assert.equal(vary.append('Accept, Accept-Encoding, *', 'Origin'), '*')
+    })
+  })
 
   describe('when field is string', function () {
     it('should set value', function () {
-      assert.equal(vary.append('', 'Accept'), 'Accept');
-    });
+      assert.equal(vary.append('', 'Accept'), 'Accept')
+    })
 
     it('should set value when vary header', function () {
-      assert.equal(vary.append('', 'Accept, Accept-Encoding'), 'Accept, Accept-Encoding');
-    });
+      assert.equal(vary.append('', 'Accept, Accept-Encoding'), 'Accept, Accept-Encoding')
+    })
 
     it('should acept LWS', function () {
-      assert.equal(vary.append('', '  Accept     ,     Origin    '), 'Accept, Origin');
-    });
+      assert.equal(vary.append('', '  Accept     ,     Origin    '), 'Accept, Origin')
+    })
 
     it('should handle contained *', function () {
-      assert.equal(vary.append('', 'Accept,*'), '*');
-    });
-  });
+      assert.equal(vary.append('', 'Accept,*'), '*')
+    })
+  })
 
   describe('when field is array', function () {
     it('should set value', function () {
-      assert.equal(vary.append('', ['Accept', 'Accept-Language']), 'Accept, Accept-Language');
-    });
+      assert.equal(vary.append('', ['Accept', 'Accept-Language']), 'Accept, Accept-Language')
+    })
 
     it('should ignore double-entries', function () {
-      assert.equal(vary.append('', ['Accept', 'Accept']), 'Accept');
-    });
+      assert.equal(vary.append('', ['Accept', 'Accept']), 'Accept')
+    })
 
     it('should be case-insensitive', function () {
-      assert.equal(vary.append('', ['Accept', 'ACCEPT']), 'Accept');
-    });
+      assert.equal(vary.append('', ['Accept', 'ACCEPT']), 'Accept')
+    })
 
     it('should handle contained *', function () {
-      assert.equal(vary.append('', ['Origin', 'User-Agent', '*', 'Accept']), '*');
-    });
+      assert.equal(vary.append('', ['Origin', 'User-Agent', '*', 'Accept']), '*')
+    })
 
     it('should handle existing values', function () {
-      assert.equal(vary.append('Accept, Accept-Encoding', ['origin', 'accept', 'accept-charset']), 'Accept, Accept-Encoding, origin, accept-charset');
-    });
-  });
-});
+      assert.equal(vary.append('Accept, Accept-Encoding', ['origin', 'accept', 'accept-charset']), 'Accept, Accept-Encoding, origin, accept-charset')
+    })
+  })
+})
 
-function alterVary(header, field) {
-  return function call(req, res) {
-    res.setHeader('Vary', header);
-    vary(res, field);
-  };
+function alterVary (header, field) {
+  return function call (req, res) {
+    res.setHeader('Vary', header)
+    vary(res, field)
+  }
 }
 
-function callVary(field) {
-  return function call(req, res) {
-    vary(res, field);
-  };
+function callVary (field) {
+  return function call (req, res) {
+    vary(res, field)
+  }
 }
 
-function createServer(fn) {
-  return http.createServer(function onRequest(req, res) {
+function createServer (fn) {
+  return http.createServer(function onRequest (req, res) {
     try {
-      fn(req, res);
-      res.statusCode = 200;
+      fn(req, res)
+      res.statusCode = 200
     } catch (err) {
-      res.statusCode = 500;
-      res.write(err.message);
+      res.statusCode = 500
+      res.write(err.message)
     } finally {
-      res.end();
+      res.end()
     }
-  });
+  })
 }
 
-function shouldNotHaveHeader(header) {
+function shouldNotHaveHeader (header) {
   return function (res) {
-    assert.ok(!(header.toLowerCase() in res.headers), 'should not have header ' + header);
-  };
+    assert.ok(!(header.toLowerCase() in res.headers), 'should not have header ' + header)
+  }
 }
